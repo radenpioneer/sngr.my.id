ca2b1fd87c21a8016d107eea488fe70eea86a393
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-12-29 09:09:55 +0000

    adjust home size

--- sngr.my.id/src/sections/home/home.tsx
+++ sngr.my.id/src/sections/home/home.tsx
@@ -7,15 +7,15 @@ const HomeSection: FC<{
   image: GetImageResult
 }> = ({ title, bio, image }) => {
   return (
-    <article className="min-h-full flex flex-col-reverse md:flex-row-reverse items-center gap-4">
-      <div className="flex flex-col gap-2 text-center md:text-left">
-        <h1 className="font-bold text-2xl md:text-4xl">{title}</h1>
+    <article className="flex flex-col-reverse items-center md:items-start gap-4 max-w-md">
+      <div className="flex flex-col gap-3 text-center md:text-left">
+        <h1 className="font-bold text-2xl md:text-5xl">{title}</h1>
         <p className="md:text-lg">{bio}</p>
       </div>
       <div>
         <img
           src={image.src}
-          className="w-[100px] h-[100px] object-cover object-center rounded-full shadow-md"
+          className="w-[80px] h-[80px] object-cover object-center rounded-full shadow-md"
           {...image.attributes}
           loading="eager"
           alt=""
