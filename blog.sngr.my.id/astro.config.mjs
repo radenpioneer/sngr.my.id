0d94936c9cfe1936174945e5c67671a31bd8bdb9
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-12-31 13:36:43 +0000

    setup keystatic (local)

--- blog.sngr.my.id/astro.config.mjs
+++ blog.sngr.my.id/astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-
+import react from '@astrojs/react'
+import markdoc from '@astrojs/markdoc'
+import keystatic from '@keystatic/astro'
 import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react(), markdoc(), keystatic()],
   output: 'hybrid',
   adapter: vercel(),
 })
---
+++ blog.sngr.my.id/keystatic.config.ts
@@ -0,0 +1,75 @@
+import { config, fields, collection, singleton } from '@keystatic/core'
+
+export default config({
+  storage: {
+    kind: 'local',
+  },
+  ui: {
+    brand: {
+      name: 'blog.sngr.my.id',
+    },
+  },
+  singletons: {
+    settings: singleton({
+      label: 'Site Settings',
+      path: 'src/content/site/site',
+      schema: {
+        title: fields.text({ label: 'Site Title' }),
+        description: fields.text({
+          label: 'Site Description',
+          multiline: true,
+        }),
+        logo: fields.image({
+          label: 'Logo',
+          directory: 'src/assets/site',
+          publicPath: '~/assets/site',
+        }),
+      },
+    }),
+  },
+  collections: {
+    posts: collection({
+      label: 'Posts',
+      slugField: 'title',
+      path: 'src/content/posts/*',
+      format: { contentField: 'content' },
+      entryLayout: 'content',
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        description: fields.text({ label: 'Description', multiline: true }),
+        date: fields.date({
+          label: 'Published Date',
+          defaultValue: { kind: 'today' },
+        }),
+        category: fields.relationship({
+          label: 'Category',
+          collection: 'categories',
+        }),
+        image: fields.image({
+          label: 'Featured Image',
+          directory: 'src/assets/posts',
+          publicPath: '~/assets/posts',
+        }),
+        content: fields.document({
+          label: 'Content',
+          formatting: true,
+          dividers: true,
+          links: true,
+          images: {
+            directory: 'src/assets/posts',
+            publicPath: '~/assets/posts',
+          },
+        }),
+      },
+    }),
+    categories: collection({
+      label: 'Category',
+      slugField: 'title',
+      path: 'src/content/categories/*',
+      schema: {
+        title: fields.slug({ name: { label: 'Title' } }),
+        description: fields.text({ label: 'Description', multiline: true }),
+      },
+    }),
+  },
+})
