cefcda706609dfbe2b044ead0fa51289e53f21f2
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2023-12-31 11:33:19 +0000

    lay out basic skeleton

---
+++ blog.sngr.my.id/tailwind.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,mdoc,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
