ee6cbea39b617d326c9e74420db082f96d384103
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-01-01 06:18:45 +0000

    create post page

--- blog.sngr.my.id/astro.config.mjs
+++ blog.sngr.my.id/astro.config.mjs
@@ -4,10 +4,22 @@ import react from '@astrojs/react'
 import markdoc from '@astrojs/markdoc'
 import keystatic from '@keystatic/astro'
 import vercel from '@astrojs/vercel/serverless'
+import sentry from '@sentry/astro'
+import spotlightjs from '@spotlightjs/astro'
+
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), markdoc(), keystatic()],
+  integrations: [
+    tailwind(),
+    react(),
+    markdoc(),
+    keystatic(),
+    sentry(),
+    spotlightjs(),
+    sitemap(),
+  ],
   output: 'hybrid',
   adapter: vercel({
     speedInsights: true,
--- blog.sngr.my.id/src/sections/home/post.tsx
+++ blog.sngr.my.id/src/sections/home/post.tsx
@@ -4,16 +4,22 @@ import { getISODate, getLocalDate } from '~/utils/date'
 import { getExcerpt } from '~/utils/excerpt'
 
 const PostSection: FC<{ post: CollectionEntry<'posts'> }> = ({ post }) => {
+  const url = [
+    post.data.category.id,
+    new Date(post.data.date).getFullYear(),
+    post.slug,
+  ].join('/')
+
   return (
     <article className="prose">
       <h2>
-        <a href="#">{post.data.title}</a>
+        <a href={url}>{post.data.title}</a>
       </h2>
       <>{getExcerpt(post.body)}</>
       <div className="not-prose flex gap-2 items-center text-xs">
         <a
           className="border border-gray-300 px-4 py-1 rounded-full hover:underline"
-          href="#"
+          href={url}
         >
           Baca Selengkapnya
         </a>
