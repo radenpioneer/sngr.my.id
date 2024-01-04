e10164a48883ee9f0c150b479036062173fa164b
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-01-04 14:16:57 +0000

    re-initialize with next.js

--- sngr.my.id/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{astro,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/typography')],
-}
