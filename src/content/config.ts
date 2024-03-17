707338671ce64777ab58d7150b95c78ebc86c211
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-03-17 00:35:43 +0000

    add basic site data

---
+++ src/content/config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from 'astro:content'
+
+export const collections = {
+  site: defineCollection({
+    type: 'data',
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      github: z.string(),
+    }),
+  }),
+}
