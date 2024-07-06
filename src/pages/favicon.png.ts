8dd773123303c34dfe1f3647127fb9516d6820bc
radenpioneer <7355835+radenpioneer@users.noreply.github.com>
2024-07-06 00:28:42 +0000

    RESET

--- src/pages/favicon.png.ts
+++
@@ -1,9 +0,0 @@
-import type { APIRoute } from 'astro'
-import { getEntry } from 'astro:content'
-
-const { data: site } = await getEntry('site', 'site')
-
-export const GET: APIRoute = async () => {
-  const response = await fetch(`https://github.com/${site.github}.png`)
-  return new Response(await response.arrayBuffer())
-}
