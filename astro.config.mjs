b77c34a81fa3c3aed72ba575044690d1ea374c2e
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-08-18 15:35:24 +0000

    setup CMS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,21 @@
 import { defineConfig } from 'astro/config'
-
+import react from '@astrojs/react'
+import keystatic from '@keystatic/astro'
 import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sngr.my.id',
+  integrations: [react(), keystatic()],
   output: 'hybrid',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+  vite: {
+    define: {
+      'process.env': process.env
+    }
+  }
 })
---
+++ src/content/posts/hello-world.md
@@ -0,0 +1,10 @@
+---
+title: Hello World!
+date: 2024-08-18
+tags:
+  - name: Contoh Tag
+    slug: contoh-tag
+lang: id
+draft: false
+---
+Selamat datang di [sngr.my.id](https://sngr.my.id). Ini adalah post pertama Anda. Edit atau hapus tulisan ini, dan Selamat nge-blog!
