30a0ce94d498ff07fcabc94031495eb659a4948a
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-08-18 12:44:22 +0000

    setup prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: 'hybrid',
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
   })
-});
+})
