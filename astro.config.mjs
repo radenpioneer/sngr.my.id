697d45eeac514cef32fbb46f93de16814fe6d746
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-08-19 15:49:26 +0000

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,7 @@ import cloudflare from '@astrojs/cloudflare'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sngr.my.id',
-  integrations: [
-    react(),
-    tailwindcss(),
-    sitemap(),
-    keystatic()
-  ],
+  integrations: [react(), tailwindcss(), sitemap(), keystatic()],
   prefetch: {
     defaultStrategy: 'tap',
     prefetchAll: true
