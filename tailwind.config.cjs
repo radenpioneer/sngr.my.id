029bd0902a58418e06c32e6b7343901b6fd4f76c
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-08-21 17:05:36 +0700

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['-apple-system', 'Inter', 'ui-sans-serif', 'system-ui', 'Arial', 'sans-serif']
+      }
+    }
   },
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')]
 }
