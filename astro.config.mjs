a7b3d45dd48d0e5acfa27086007d794014a48072
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-08-28 12:38:29 +0700

    Update CSP rules (#24)
    * Update _headers
    
    * Update astro.config.mjs
    
    * Update _headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://sngr.my.id',
+  site: 'https://www.sngr.my.id',
   integrations: [
     react(),
     tailwindcss(),
